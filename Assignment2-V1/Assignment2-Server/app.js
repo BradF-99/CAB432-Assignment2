7377e6480fa7045c1e2a3828a9e17c3344b4396e
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-10-19 13:08:37 +1000

    Added helmet to app core

--- Assignment2-V1/Assignment2-Server/app.js
+++ Assignment2-V1/Assignment2-Server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 const twitterRouter = require('./routes/twitterRouter');
@@ -14,6 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
