a935df4b9e1bad94611b73e4e848865ae4c96d48
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-10-26 22:02:33 +1000

    Fixed CORS

--- app.js
+++ app.js
@@ -10,16 +10,18 @@ const twitterRouter = require('./routes/twitterRouter');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/twitter', twitterRouter);
