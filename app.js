ce9c578febe5c9a019f9c4f40c6dae450457532d
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-10-27 00:37:59 +1000

    Added pug as view engine

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const twitterRouter = require('./routes/twitterRouter');
 
 const app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use(logger('dev'));
 app.use(helmet());
 app.use(express.json());
