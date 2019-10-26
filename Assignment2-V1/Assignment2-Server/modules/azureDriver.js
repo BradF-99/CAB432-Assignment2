3e1f3d743a6133b5a68a3bf6593627212eacab4d
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-10-26 18:08:31 +1000

    Added exception catching to Azure driver

--- Assignment2-V1/Assignment2-Server/modules/azureDriver.js
+++ Assignment2-V1/Assignment2-Server/modules/azureDriver.js
@@ -4,9 +4,7 @@ const {
   ContainerURL,
   ServiceURL,
   SharedKeyCredential,
-  StorageURL,
-  uploadStreamToBlockBlob,
-  uploadFileToBlockBlob
+  StorageURL
 } = require("@azure/storage-blob");
 
 const STORAGE_ACCOUNT_NAME = "cab432assignment2data";
@@ -16,7 +14,7 @@ const credentials = new SharedKeyCredential(STORAGE_ACCOUNT_NAME, ACCOUNT_ACCESS
 const pipeline = StorageURL.newPipeline(credentials);
 
 const serviceURL = new ServiceURL(`https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net`, pipeline);
-const containerURL = ContainerURL.fromServiceURL(serviceURL, "twitterData");
+const containerURL = ContainerURL.fromServiceURL(serviceURL, "twitterdata");
 
 const aborter = Aborter.timeout(30 * 60 * 1000); // 30 second timeout
 
@@ -34,19 +32,23 @@ async function streamToString(readableStream) {
   });
 }
 
+// return list of blobs from azure
 async function returnBlobNames() {
   let marker = undefined;
   let names = [];
 
-  do {
-    const listBlobsResponse = await containerURL.listBlobFlatSegment(aborter, marker);
-    marker = listBlobsResponse.nextMarker;
-    for (const blob of listBlobsResponse.segment.blobItems) {
-      names.push(blob.name);
-    }
-  } while (marker);
-
-  return (names);
+  try {
+    do {
+      const listBlobsResponse = await containerURL.listBlobFlatSegment(aborter, marker);
+      marker = listBlobsResponse.nextMarker;
+      for (const blob of listBlobsResponse.segment.blobItems) {
+        names.push(blob.name);
+      }
+    } while (marker);
+    return (names);
+  } catch (e) {
+    throw (e);
+  }
 }
 
 async function uploadBlob(content, blobName) {
@@ -61,12 +63,13 @@ async function uploadBlob(content, blobName) {
 async function downloadBlob(blobName) {
   const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, blobName);
 
-  const downloadResponse = await blockBlobURL.download(aborter, 0);
-  const downloadedContent = await streamToString(downloadResponse.readableStreamBody);
-
-  const contentString = await streamToString(downloadedContent);
-
-  return contentString;
+  try {
+    const downloadResponse = await blockBlobURL.download(aborter, 0);
+    const downloadedContent = await streamToString(downloadResponse.readableStreamBody);
+    return downloadedContent;
+  } catch (e) {
+    throw (e);
+  }
 }
 
 module.exports = {
